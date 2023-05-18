2dab381598a177f1da8dda5e30994a3894aa6af4
Sulaiman Amjad <sulaiman.amjad@gmail.com>
2023-05-18 09:41:48 +0100

    Added dashboard page and css. Commented out global css dark theme

---
+++ app/dashboard/page.js
@@ -0,0 +1,9 @@
+import styles from './dashboard.module.css';
+
+export default function DashBoard(){
+    return (
+        <div className={styles.dashboard}>
+            <div className={styles.fraction}></div>
+        </div>
+    )
+}
