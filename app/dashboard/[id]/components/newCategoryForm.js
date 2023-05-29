d45ef590ef3454fdbd96f180326beca8f7cf40af
Sulaiman Amjad <sulaiman.amjad@gmail.com>
2023-05-29 10:55:37 +0100

    Refactored new-category form into its own component.

---
+++ app/dashboard/[id]/components/newCategoryForm.js
@@ -0,0 +1,17 @@
+import { useState } from "react";
+
+export default function NewCategoryForm (props){
+    const [showForm, setShowForm] = useState(false)
+    const [title, setTitle] = useState();
+    const [data, setData] = useState();
+    
+    return (
+        <>
+        {showForm ? <>
+            <input onChange={(e)=> setTitle(e.target.value)} placeholder='Category'></input>
+            <input onChange={(e)=>setData({...data, budget:e.target.value})} placeholder='Budget'></input>
+            <button onClick={() => props.handleAddCategory(title, data)}>add</button>
+        </> : <button onClick={() => setShowForm(true)}>+</button>}
+        </>
+    )
+}
--- app/dashboard/[id]/page.js
+++ app/dashboard/[id]/page.js
@@ -6,18 +6,19 @@ import { addCategory } from '../../firebase/setData';
 import { useState, useEffect } from 'react';
 
 import BudgetBreakdownNavButton from '../budgetBreakdownNavButton';
+import NewCategoryForm from './components/newCategoryForm';
 import useFirestoreRealtimeUpdate from '../../firebase/useFirestoreRealtimeUpdate';
-import { getCategoryCollectionRef } from '@/app/firebase/getFirestoreRef';
+import { getCategoryCollectionRef, getTripRef } from '@/app/firebase/getFirestoreRef';
 
 export default function DashBoard({params}){
     console.log('params', params)
 
     const [budget, setBudget] = useState();
+    const [remainingBudget, setRemainingBudget] = useState();
     const [destination, setDestination] = useState();
-    const [addingCategory, setAddingCategory] = useState(false);
-    const [newCategory, setNewCategory] = useState({title:'new', data:{budget:'100'}});
     
     const categoryDocs = useFirestoreRealtimeUpdate(getCategoryCollectionRef(params.id));
+    // const tripData = useFirestoreRealtimeUpdate(getTripRef(params.id));
 
     useEffect(()=>{
         getDocument('journey', params.id)
@@ -28,29 +29,31 @@ export default function DashBoard({params}){
         });
     }, []);
 
+    // useEffect(() => {
+    //     setRemainingBudget(categoryDocs.map(doc => doc.data().budget).reduce((a,b)=>{
+    //         return parseFloat(a)+parseFloat(b)}, 0));
+    //     console.log('remainingbudget', remainingBudget);
+    //     console.log(tripData);
+    // }, [tripData])
+
     const handleEdit = async (category, budget) => {
         addCategory(params.id, category, {budget})
     }
     
-    const handleAddCategory = async (event) => {
-        addCategory(params.id, newCategory.title, newCategory.data);
+    const handleAddCategory = async (title, data) => {
+        addCategory(params.id, title, data);
     }
 
     return (
         <div className={styles.dashboard}>
-            <div>{newCategory.title}</div>
-            <div>{newCategory.data.budget}</div>
+            {/* <div>{`Remaining budget ${remainingBudget}`}</div> */}
             {/* <div className={styles.fraction}></div> */}
             <div>Budget: {budget}</div><button>Edit</button><input></input>
             <div>Destination: {destination}</div><button>Edit</button>
             {categoryDocs ? categoryDocs.map((doc, index)=> 
                 <BudgetBreakdownNavButton key={`bbnb${index}`} category={doc.id} budget={doc.data().budget} handleEdit = {handleEdit}/>
             ) : console.log(categoryDocs)}
-            {addingCategory ? <>
-                <input onChange={(e)=> setNewCategory({...newCategory, title:e.target.value})} placeholder='Category'></input>
-                <input onChange={(e)=>setNewCategory({...newCategory, data:{...newCategory.data, budget:e.target.value}})} placeholder='Budget'></input>
-                <button onClick={() => handleAddCategory()}>add</button>
-            </> : <button onClick={() => setAddingCategory(true)}>+</button>}
+            <NewCategoryForm handleAddCategory={handleAddCategory}/>
         </div>
     )
 }
