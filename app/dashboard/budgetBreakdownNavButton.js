ef9df1cb4c70107806af98ab70fe01f110c64d7d
Sulaiman Amjad <sulaiman.amjad@gmail.com>
2023-06-06 15:50:21 +0100

    Added modals for editing category info

--- app/dashboard/budgetBreakdownNavButton.js
+++ app/dashboard/budgetBreakdownNavButton.js
@@ -1,5 +1,6 @@
 import styles from './nav_budget.module.css';
-import { useState } from 'react';
+import { useState, useRef } from 'react';
+import { Modal } from '../components/modal';
 
 export default function BudgetBreakdownNavButton (props){
     console.log('category id', props.id, 'category', props.category, 'budget', props.budget)
@@ -7,17 +8,21 @@ export default function BudgetBreakdownNavButton (props){
     const [newCategory, setNewCategory] = useState(props.category);
     const [newBudget, setNewBudget] = useState(props.budget);
 
+    const categoryDiv = useRef();
+
     return (
         <div>
-        <div className={styles.button_container}>
+        <div ref={categoryDiv} className={styles.button_container}>
             <div className={styles.button_title}>{props.category ? props.category : "Accomodation"}</div>
             <div className={styles.button_budget}>{props.budget ? `£${props.budget}` : "£1000"}</div>
         </div>
+        <Modal modalTrigger={categoryDiv}>
         <div>
             <input defaultValue = {props.category} onChange={(e) => {setNewCategory(e.target.value)}}/>
             <input defaultValue = {props.budget} onChange={(e) => {setNewBudget(e.target.value)}}/>
             <button onClick={() => props.handleEdit(props.id, {name:newCategory, budget:newBudget})}>Save</button>
         </div>
+        </Modal>
         <button onClick={()=>props.handleDelete(props.id)}>Delete Category</button>
         </div>
     )
