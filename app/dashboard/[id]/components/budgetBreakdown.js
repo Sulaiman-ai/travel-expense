4b765db4ae1dac3cb2fdeb4a66fff77e20ec31f3
Sulaiman Amjad <sulaiman.amjad@gmail.com>
2023-07-14 17:34:20 +0100

    displaying transactions made under each category when clicked

--- app/dashboard/[id]/components/budgetBreakdown.js
+++ app/dashboard/[id]/components/budgetBreakdown.js
@@ -3,8 +3,10 @@ import { addCategory, editCategory } from '@/app/firebase/setData';
 import { deleteCategory } from '@/app/firebase/deleteData';
 
 import styles from '../components/budgetbreakdownnavbutton/nav_budget.module.css';
+import { useState } from 'react';
 
 export default function BudgetBreakdown(props){
+    const [edit, setEdit] = useState(false);
 
     const handleEdit = async (category_id, data) => {
         editCategory(props.trip_id, category_id, data)
@@ -15,10 +17,13 @@ export default function BudgetBreakdown(props){
     }
 
     return (
+        <>
+        <button onClick={() => {setEdit(!edit)}}>Edit</button>
         <div className={styles.category_grid}>
         {props.categoryDocs ? props.categoryDocs.map((doc, index)=> 
-            <BudgetBreakdownNavButton key={`bbnb${index}`} id={doc.id} category={doc.data().name} budget={doc.data().budget} handleEdit = {handleEdit} handleDelete={handleDelete}/>
+            <BudgetBreakdownNavButton key={`bbnb${index}`} id={doc.id} edit={edit} category={doc.data().name} budget={doc.data().budget} transactions={doc.data()?.transactions} handleEdit = {handleEdit} handleDelete={handleDelete}/>
         ) : console.log(props.categoryDocs)}
         </div>
+        </>
     )
 }
--- app/dashboard/[id]/components/budgetbreakdownnavbutton/budgetBreakdownNavButton.js
+++ app/dashboard/[id]/components/budgetbreakdownnavbutton/budgetBreakdownNavButton.js
@@ -1,13 +1,25 @@
 import styles from './nav_budget.module.css';
 import formStyles from '../../../../components/tripform/newtripform.module.css';
-import { useState, useRef } from 'react';
+import { useState, useRef, useEffect } from 'react';
 import { Modal } from '../../../../components/modal/modal';
+import TransactionDisplay from '@/app/transactions/components/transactiondisplay/transactionDisplay';
+import { getDoc } from 'firebase/firestore';
 
 export default function BudgetBreakdownNavButton (props){
-    console.log('category id', props.id, 'category', props.category, 'budget', props.budget)
+    // console.log('category id', props.id, 'category', props.category, 'budget', props.budget)
     const [expanded, setExpanded] = useState(false);
     const [newCategory, setNewCategory] = useState(props.category);
     const [newBudget, setNewBudget] = useState(props.budget);
+
+    const [transactionsDocs, setTransactionsDocs] = useState();
+
+    useEffect(() => {
+            props.transactions ? Promise.all(props.transactions?.map(async (transaction)=> {
+                let doc = await getDoc(transaction);
+                return doc;
+            })).then((docs) => {
+                setTransactionsDocs(docs)}) : null;
+    }, []);
 
     const categoryDiv = useRef();
 
@@ -18,11 +30,18 @@ export default function BudgetBreakdownNavButton (props){
             <div className={styles.button_budget}>{props.budget ? `£${props.budget}` : "£1000"}</div>
         </div>
         <Modal modalTrigger={categoryDiv}>
+        {props.edit ? 
         <div className={formStyles.container}>
             <input className={formStyles.input} defaultValue = {props.category} onChange={(e) => {setNewCategory(e.target.value)}}/>
             <input className={formStyles.input} defaultValue = {props.budget} onChange={(e) => {setNewBudget(e.target.value)}}/>
             <button onClick={() => props.handleEdit(props.id, {name:newCategory, budget:newBudget})}>Save</button>
-        </div>
+        </div> :
+        <div>
+            <h1>Transactions</h1>
+            {transactionsDocs ? transactionsDocs.map((doc) => <TransactionDisplay {...doc.data()}/>):null}
+            {/* {transactionsDocs ? transactionsDocs?.map((doc) => <p>{doc.data().transaction}</p>):null} */}
+            {/* {props.transactions?.map(async (transaction) => <p>{await getDoc(transaction).data().name}</p>)} */}
+        </div>}
         </Modal>
         {/* <button onClick={()=>props.handleDelete(props.id)}>Delete Category</button> */}
         </div>
