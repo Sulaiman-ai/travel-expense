9ba7e85db947a00dc0413840e929a173be2d9737
Sulaiman Amjad <sulaiman.amjad@gmail.com>
2023-08-22 19:26:25 +0100

    New category form inputs are required and form submission clears inputs

--- app/dashboard/[id]/components/newCategoryForm.js
+++ app/dashboard/[id]/components/newCategoryForm.js
@@ -7,11 +7,15 @@ export default function NewCategoryForm (props){
     
     return (
         <>
-        {showForm ? <>
-            <input onChange={(e)=> setData({...data, name:e.target.value})} placeholder='Category'></input>
-            <input onChange={(e)=>setData({...data, budget:e.target.value})} placeholder='Budget'></input>
-            <button onClick={() => props.handleAddCategory(title, data)}>add</button>
-        </> : <button onClick={() => setShowForm(true)}>+</button>}
+        {showForm ? <form onSubmit={(event) => {
+            event.preventDefault();
+            props.handleAddCategory(title, data)
+            event.target.reset();
+            }}>
+            <input required onChange={(e)=> setData({...data, name:e.target.value})} placeholder='Category'></input>
+            <input required onChange={(e)=>setData({...data, budget:e.target.value})} placeholder='Budget'></input>
+            <button type="submit">add</button>
+        </form> : <button onClick={() => setShowForm(true)}>+</button>}
         </>
     )
 }
