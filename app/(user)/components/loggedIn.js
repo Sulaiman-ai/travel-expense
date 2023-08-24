fb27cc30a7346b42d6615038a32232685b7c3ae9
Sulaiman Amjad <sulaiman.amjad@gmail.com>
2023-08-24 10:07:51 +0100

    Checks whether user is on signup page before trying to redirect to signup page (Fixes constant refreshing problem)

--- app/(user)/components/loggedIn.js
+++ app/(user)/components/loggedIn.js
@@ -16,7 +16,7 @@ export default function useLoggedIn (){
         if (!loading && !authUser) {
             router.push('/signup')
         } 
-        else if (authUser && pathname == '/signup') {
+        else if (authUser && pathname == '/signup' && pathname !== '/signup') {
             router.push('/')
         }
     }, [authUser, loading, pathname]);
